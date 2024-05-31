0e58bf3173b8e6dcbca51a2cb1f5e2c5be46bf3e
Silva <carlos.uchoa.d.silva@accenture.com>
2024-05-30 21:29:04 -0300

    solution of poo project is complete

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Testando Nokia:");
+        Smartphone nokia = new Nokia("123456789", "Nokia Modelo X", "111222333444", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTestando iPhone:");
+        Smartphone iphone = new Iphone("987654321", "iPhone Modelo Y", "555666777888", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
